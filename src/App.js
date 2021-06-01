98d0a9da2a819350a4f45d819221f2d79a5d6a09
uatiintellect <uatiintellect@gmail.com>
2021-06-01 20:57:47 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Test from "./test"
+
+export default function IndexPage() {
+  return (
+    <>
+      <Test />
+    </>
+  );
+}
+
---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import renderer from 'react-test-renderer'
+
+describe('snapshot testing', () => {
+
+  test('snapshot for App Components', () => {
+  
+    const renderdComponent = renderer.create(<App />).toJSON()
+    expect(renderdComponent).toMatchSnapshot()
+})
+})
